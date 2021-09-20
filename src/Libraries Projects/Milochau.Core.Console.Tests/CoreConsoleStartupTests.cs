69478b5bed16cefc2e63c89fd2201cb91a50d87f
Antoine Milochau <48644548+amilochau@users.noreply.github.com>
2021-09-20 17:04:01 +0200

    Add more unit tests on Console (#51)

---
+++ src/Libraries Projects/Milochau.Core.Console.Tests/CoreConsoleStartupTests.cs
@@ -0,0 +1,51 @@
+﻿using Milochau.Core.Abstractions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using Microsoft.FeatureManagement;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Collections.Generic;
+using Milochau.Core.Console;
+
+namespace Milochau.Core.Functions.Tests
+{
+    [TestClass]
+    public class CoreConsoleStartupTests
+    {
+        [TestMethod]
+        public void ConfigureServices_Should_RegisterAllServices_When_Called()
+        {
+            // Arrange
+            var serviceCollection = new ServiceCollection();
+
+            var configurationBuilder = new ConfigurationBuilder();
+            configurationBuilder.AddInMemoryCollection(new Dictionary<string, string>
+            {
+                { "AZURE_FUNCTIONS_APPCONFIG_ENDPOINT", "https://" },
+                { "AZURE_FUNCTIONS_KEYVAULT_VAULT", "https://xxx.vault.azure.net" }
+            });
+
+            var configuration = configurationBuilder.Build();
+            serviceCollection.AddSingleton<IConfiguration>(configuration);
+            serviceCollection.AddSingleton(Mock.Of<IHostEnvironment>());
+
+            var startup = CoreConsoleStartup.Create<Startup>(configuration);
+
+            // Act
+            startup.ConfigureServices(serviceCollection);
+
+            // Assert
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            Assert.IsNotNull(serviceProvider.GetService<IOptions<CoreHostOptions>>());
+            Assert.IsNotNull(serviceProvider.GetService<IApplicationHostEnvironment>());
+            Assert.IsNotNull(serviceProvider.GetService<IFeatureManager>());
+        }
+
+        public class Startup : CoreConsoleStartup
+        {
+        }
+    }
+}
--- src/Reference Projects/Milochau.Core.Console.ReferenceProject.Tests/EntryPointRegistrationTests.cs
+++ src/Reference Projects/Milochau.Core.Console.ReferenceProject.Tests/EntryPointRegistrationTests.cs
@@ -4,6 +4,8 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using System;
 
 namespace Milochau.Core.Console.ReferenceProject.Tests
 {
@@ -11,7 +13,7 @@ namespace Milochau.Core.Console.ReferenceProject.Tests
     public partial class EntryPointRegistrationTests
     {
         [TestMethod]
-        public async Task Should_CallEntryPoint_When_HostIsStarted()
+        public async Task Should_CallTestEntryPoint_When_HostIsStarted()
         {
             // Arrange
             var businessService = new Mock<IBusinessService>();
@@ -28,6 +30,31 @@ namespace Milochau.Core.Console.ReferenceProject.Tests
 
             // Assert
             businessService.Verify(x => x.Call(), Times.Once);
+        }
+
+        [TestMethod]
+        public async Task Should_CallEntryPoint_When_HostIsStarted()
+        {
+            // Arrange
+            var logger = new Mock<ILogger<EntryPoint>>();
+
+            var hostBuilder = new HostBuilder()
+                .ConfigureCoreHostBuilder<Startup, EntryPoint>()
+                .ConfigureServices(services =>
+                {
+                    services.AddSingleton<ILogger<EntryPoint>>(logger.Object);
+                });
+
+            // Act
+            await hostBuilder.RunConsoleAsync();
+
+            // Assert
+            logger.Verify(x => x.Log(
+                It.IsAny<LogLevel>(),
+                It.IsAny<EventId>(),
+                It.IsAny<It.IsAnyType>(),
+                It.IsAny<Exception>(),
+                It.IsAny<Func<It.IsAnyType, Exception, string>>()), Times.AtLeastOnce);
         }
     }
 }
