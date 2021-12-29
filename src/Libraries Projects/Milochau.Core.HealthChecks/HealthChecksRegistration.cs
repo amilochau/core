241f65cde154ba7cb8a604f09dd307c4994627ba
Antoine Milochau <48644548+amilochau@users.noreply.github.com>
2021-12-29 10:55:33 +0100

    Add a specific health check for Application Host Environment configuration

--- src/Libraries Projects/Milochau.Core.HealthChecks/HealthChecksRegistration.cs
+++ src/Libraries Projects/Milochau.Core.HealthChecks/HealthChecksRegistration.cs
@@ -24,6 +24,17 @@ namespace Milochau.Core.HealthChecks
             // Add default endpoint health check
             healthChecksBuilder.AddCheck("Endpoint", () => HealthCheckResult.Healthy(), new[] { LightTag });
 
+            // Add Application Host Environment health check
+            healthChecksBuilder.AddCheck("Application Host Environment", () => {
+                return string.IsNullOrWhiteSpace(hostOptions.Application.OrganizationName)
+                    || string.IsNullOrWhiteSpace(hostOptions.Application.ApplicationName)
+                    || string.IsNullOrWhiteSpace(hostOptions.Application.EnvironmentName)
+                    || string.IsNullOrWhiteSpace(hostOptions.Application.HostName)
+                    || string.IsNullOrWhiteSpace(hostOptions.Application.RegionName)
+                    ? HealthCheckResult.Degraded("Application Host Environment is partially set. You should add app settings for Organization, Application, Environment, Host and Region.")
+                    : HealthCheckResult.Healthy();
+            }, new[] { LightTag });
+
             // Add Azure Key Vault health check
             if (!string.IsNullOrEmpty(hostOptions.KeyVault.Vault))
             {
