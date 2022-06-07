80dbcc1b063b39223c02a3540dacbef23daad74d
Antoine Milochau <48644548+amilochau@users.noreply.github.com>
2022-06-07 12:19:00 +0200

    Fix unit tests

---
+++ src/Libraries Projects/Milochau.Core.AspNetCore.Tests/TestHelpers/ObsoleteHostingEnvironment.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.FileProviders;
+
+namespace Milochau.Core.AspNetCore.Tests.TestHelpers
+{
+#pragma warning disable CS0618 // Le type ou le membre est obsolète
+    internal class ObsoleteHostingEnvironment : IHostingEnvironment
+#pragma warning restore CS0618 // Le type ou le membre est obsolète
+    {
+        public string EnvironmentName { get; set; }
+        public string ApplicationName { get; set; }
+        public string WebRootPath { get; set; }
+        public IFileProvider WebRootFileProvider { get; set; }
+        public string ContentRootPath { get; set; }
+        public IFileProvider ContentRootFileProvider { get; set; }
+    }
+}
