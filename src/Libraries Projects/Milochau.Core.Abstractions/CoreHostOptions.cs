ded85859008032fe82e6b197ddf84573918f362d
Antoine Milochau <48644548+amilochau@users.noreply.github.com>
2022-07-04 16:45:34 +0200

    Add three first infrastructure conventions (Storage account & Cosmos DB) (#84)
    * Add three first infrastructure conventions (Storage account & Cosmos DB)
    
    * Update version

--- src/Libraries Projects/Milochau.Core.Abstractions/CoreHostOptions.cs
+++ src/Libraries Projects/Milochau.Core.Abstractions/CoreHostOptions.cs
@@ -1,4 +1,5 @@
 ﻿using Azure.Identity;
+using System;
 
 namespace Milochau.Core.Abstractions
 {
@@ -35,12 +36,41 @@ namespace Milochau.Core.Abstractions
 
         /// <summary>Region name</summary>
         public string RegionName { get; set; }
-    }
 
+        /// <summary>Get the infrastructure convention for a defined type</summary>
+        public string GetInfrastructureConvention(InfrastructureConventionType infrastructureConventionType, string suffix = "")
+        {
+            switch (infrastructureConventionType)
+            {
+                case InfrastructureConventionType.StorageAccountUri:
+                    return $"{OrganizationName}{ApplicationName}{HostName}sto{suffix}.blob.core.windows.net/";
+                case InfrastructureConventionType.CosmosDbDatabaseName:
+                    return $"{OrganizationName}-{ApplicationName}-{HostName}-cosmosdb";
+                case InfrastructureConventionType.CosmosDbAccountEndpoint:
+                    return $"https://{OrganizationName}-{ApplicationName}-{HostName}-cosmos.documents.azure.com:443/";
+                default:
+                    throw new NotSupportedException("Infrastructure convention type not supported!");
+            }
+        }
+    }
+    
     /// <summary>Azure Key Vault options</summary>
     public class KeyVaultOptions
     {
         /// <summary>Vault URI</summary>
         public string Vault { get; set; }
+    }
+
+    /// <summary>Type of infrastructure convention</summary>
+    public enum InfrastructureConventionType
+    {
+        /// <summary>Storage account URI</summary>
+        StorageAccountUri = 1,
+
+        /// <summary>Cosmos DB database name</summary>
+        CosmosDbDatabaseName = 2,
+
+        /// <summary>Cosmos DB account endpoint</summary>
+        CosmosDbAccountEndpoint = 3
     }
 }
