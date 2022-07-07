9e2a314f1e7cc50c1717e27550b3ca7d0583572e
Antoine Milochau <48644548+amilochau@users.noreply.github.com>
2022-07-07 19:32:11 +0200

    Create a new library with Cosmos Client registration & extensions (#89)

---
+++ src/Milochau.Core.Cosmos/Helpers/CosmosClientExtensions.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Cosmos.Linq;
+using Microsoft.Extensions.Logging;
+using Milochau.Core.Abstractions.Exceptions;
+using Milochau.Finance.Helpers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Milochau.Core.Cosmos.Helpers
+{
+    /// <summary>Extensions for <see cref="CosmosClient"/></summary>
+    public static class CosmosClientExtensions
+    {
+        /// <summary>Exception message when the entity is not found</summary>
+        public const string EntityNotFoundExceptionMessage = "Entity is not found.";
+
+        /// <summary>Create a new entity</summary>
+        /// <typeparam name="TItem">Type of database entity</typeparam>
+        public async static Task CreateItemAsync<TItem>(this CosmosClient cosmosClient, string databaseName, string containerName, TItem item, string partitionKey, ILogger logger, CancellationToken cancellationToken)
+        {
+            var container = cosmosClient.GetContainer(databaseName, containerName);
+            var response = await container.CreateItemAsync(item, new PartitionKey(partitionKey), null, cancellationToken);
+
+            logger.LogResponse(response, "create");
+        }
+
+        /// <summary>Read an entity as a point item</summary>
+        /// <typeparam name="TItem">Type of database entity</typeparam>
+        /// <exception cref="NotFoundException">Entity has not been found</exception>
+        public async static Task<TItem> ReadPointItemAsync<TItem>(this CosmosClient cosmosClient, string databaseName, string containerName, string id, string partitionKey, ILogger logger, CancellationToken cancellationToken)
+            where TItem : IEntity<string>
+        {
+            try
+            {
+                var container = cosmosClient.GetContainer(databaseName, containerName);
+                var response = await container.ReadItemAsync<TItem>(id, new PartitionKey(partitionKey), null, cancellationToken);
+
+                logger.LogResponse(response, "read point");
+
+                return response.Resource;
+            }
+            catch (CosmosException ex) when (ex.StatusCode == System.Net.HttpStatusCode.NotFound)
+            {
+                throw new NotFoundException(EntityNotFoundExceptionMessage);
+            }
+        }
+
+        /// <summary>Query entities</summary>
+        /// <typeparam name="TItem">Type of database entity</typeparam>
+        public static IOrderedQueryable<TItem> QueryItems<TItem>(this CosmosClient cosmosClient, string databaseName, string containerName, string? partitionKey)
+            where TItem : IEntity<string>
+        {
+            var container = cosmosClient.GetContainer(databaseName, containerName);
+            return container.GetItemLinqQueryable<TItem>(false, null, new QueryRequestOptions
+            {
+                PartitionKey = partitionKey != null ? new PartitionKey(partitionKey) : null,
+            });
+        }
+
+        /// <summary>Get a single entity from a query</summary>
+        /// <typeparam name="TItem">Type of database entity</typeparam>
+        /// <exception cref="NotFoundException">Entity has not been found or is not unique</exception>
+        public async static Task<TItem> GetSingleItemAsync<TItem>(this IQueryable<TItem> query, ILogger logger, CancellationToken cancellationToken)
+        {
+            using var feedIterator = query.ToFeedIterator();
+
+            // Only read one item, so we don't need to loop with the 'feedIterator.HasMoreResults'
+            var response = await feedIterator.ReadNextAsync(cancellationToken);
+
+            logger.LogResponse(response, "get single");
+
+            if (response.Count != 1)
+            {
+                throw new NotFoundException(EntityNotFoundExceptionMessage);
+            }
+
+            return response.First();
+        }
+
+        /// <summary>List entities from a query</summary>
+        /// <typeparam name="TItem">Type of database entity</typeparam>
+        public async static IAsyncEnumerable<TItem> ListItemsAsync<TItem>(this IQueryable<TItem> query, ILogger logger, [EnumeratorCancellation] CancellationToken cancellationToken)
+        {
+            using var feedIterator = query.ToFeedIterator();
+
+            // Iterate query result pages
+            while (feedIterator.HasMoreResults)
+            {
+                var response = await feedIterator.ReadNextAsync(cancellationToken);
+                logger.LogResponse(response, "list");
+
+                foreach (var item in response)
+                {
+                    yield return item;
+                }
+            }
+        }
+
+        /// <summary>Patch an entity</summary>
+        /// <typeparam name="TItem">Type of database entity</typeparam>
+        public async static Task PatchItemAsync<TItem>(this CosmosClient cosmosClient, string databaseName, string containerName, string id, string partitionKey, IReadOnlyList<PatchOperation> patchOperations, ILogger logger, CancellationToken cancellationToken)
+        {
+            var container = cosmosClient.GetContainer(databaseName, containerName);
+            var response = await container.PatchItemAsync<TItem>(id, new PartitionKey(partitionKey), patchOperations, null, cancellationToken);
+
+            logger.LogResponse(response, "patch");
+        }
+
+        /// <summary>Remove an entity</summary>
+        /// <typeparam name="TItem">Type of database entity</typeparam>
+        public async static Task RemoveItemAsync<TItem>(this CosmosClient cosmosClient, string databaseName, string containerName, string id, string partitionKey, ILogger logger, CancellationToken cancellationToken)
+        {
+            var container = cosmosClient.GetContainer(databaseName, containerName);
+            var response = await container.DeleteItemAsync<TItem>(id, new PartitionKey(partitionKey), null, cancellationToken);
+
+            logger.LogResponse(response, "remove");
+        }
+
+        private static void LogResponse<TItem>(this ILogger logger, ItemResponse<TItem> response, string operationType)
+        {
+            logger.LogInformation($"{typeof(TItem)} - {operationType} - RequestCharge: {response.RequestCharge}");
+            logger.LogInformation(response.Diagnostics.ToString());
+        }
+        private static void LogResponse<TItem>(this ILogger logger, FeedResponse<TItem> response, string operationType)
+        {
+            logger.LogInformation($"{typeof(TItem)} - {operationType} - RequestCharge: {response.RequestCharge}");
+            logger.LogInformation(response.Diagnostics.ToString());
+        }
+    }
+}
---
+++ src/Milochau.Core.Cosmos/ServiceCollectionExtensions.cs
@@ -0,0 +1,53 @@
+﻿using Azure.Core;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.DependencyInjection;
+using Milochau.Core.Abstractions;
+using Milochau.Core.Cosmos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Milochau.Core.Cosmos
+{
+    /// <summary>Services registration extensions</summary>
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>Add Cosmos DB services</summary>
+        public static IServiceCollection AddCosmosDb(IServiceCollection services, Action<CosmosDbSettings> settings)
+        {
+            var settingsValue = new CosmosDbSettings();
+            settings.Invoke(settingsValue);
+
+            services.AddSingleton<CosmosClient>(serviceProvider =>
+            {
+                var applicationHostEnvironment = serviceProvider.GetRequiredService<IApplicationHostEnvironment>();
+                var credential = serviceProvider.GetRequiredService<TokenCredential>();
+
+                var cosmosClientOptions = new CosmosClientOptions
+                {
+                    ApplicationName = applicationHostEnvironment.ApplicationName,
+                    EnableContentResponseOnWrite = false,
+                    SerializerOptions = new CosmosSerializationOptions
+                    {
+                        IgnoreNullValues = true,
+                        Indented = false,
+                        PropertyNamingPolicy = CosmosPropertyNamingPolicy.CamelCase
+                    }
+                };
+
+                if (!string.IsNullOrEmpty(settingsValue.ConnectionString))
+                {
+                    return new CosmosClient(settingsValue.ConnectionString, cosmosClientOptions);
+                }
+                else
+                {
+                    return new CosmosClient(settingsValue.AccountEndpoint, credential, cosmosClientOptions);
+                }
+            });
+
+            return services;
+        }
+    }
+}
