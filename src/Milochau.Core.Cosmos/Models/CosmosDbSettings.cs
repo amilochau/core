8eeb7a915b82597706ed5e02f657eea7bd3652bf
Antoine Milochau <48644548+amilochau@users.noreply.github.com>
2022-07-07 19:31:43 +0200

    Create a new library with Cosmos Client registration & extensions

---
+++ src/Milochau.Core.Cosmos/Models/CosmosDbSettings.cs
@@ -0,0 +1,24 @@
+﻿using Azure.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Milochau.Core.Cosmos.Models
+{
+    /// <summary>Cosmos DB settings</summary>
+    public class CosmosDbSettings
+    {
+
+        /// <summary>Connection string</summary>
+        /// <remarks>If no connection string is set, we try to authenticate with <see cref="AccountEndpoint"/> and <see cref="TokenCredential"/></remarks>
+        public string? ConnectionString { get; set; }
+
+        /// <summary>Account endpoint</summary>
+        public string AccountEndpoint { get; set; } = null!;
+
+        /// <summary>Database name</summary>
+        public string DatabaseName { get; set; } = null!;
+    }
+}
