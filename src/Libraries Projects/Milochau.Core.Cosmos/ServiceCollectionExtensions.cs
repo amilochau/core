b8e95146c1b491ebe23b1a5e7cd129685ea81ead
Antoine Milochau <48644548+amilochau@users.noreply.github.com>
2022-07-11 13:03:56 +0200

    Fix CosmosClient registration

--- src/Libraries Projects/Milochau.Core.Cosmos/ServiceCollectionExtensions.cs
+++ src/Libraries Projects/Milochau.Core.Cosmos/ServiceCollectionExtensions.cs
@@ -1,13 +1,11 @@
 ﻿using Azure.Core;
 using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using Milochau.Core.Abstractions;
 using Milochau.Core.Cosmos.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Milochau.Core.Cosmos
 {
@@ -15,15 +13,28 @@ namespace Milochau.Core.Cosmos
     public static class ServiceCollectionExtensions
     {
         /// <summary>Add Cosmos DB services</summary>
-        public static IServiceCollection AddCosmosDb(this IServiceCollection services, Action<CosmosDbSettings> settings)
+        public static IServiceCollection AddCosmosDb(this IServiceCollection services, Action<CosmosDbSettings>? settings = null)
         {
-            var settingsValue = new CosmosDbSettings();
-            settings.Invoke(settingsValue);
+            if (settings != null)
+            {
+                services.Configure<CosmosDbSettings>(settings);
+            }
+            else
+            {
+                services.AddOptions<CosmosDbSettings>()
+                    .Configure<IConfiguration>((options, configuration) => configuration.GetSection("Database").Bind(options))
+                    .PostConfigure<IOptions<CoreHostOptions>>((options, hostOptions) =>
+                    {
+                        options.DatabaseName ??= hostOptions.Value.Application.GetInfrastructureConvention(InfrastructureConventionType.CosmosDbDatabaseName);
+                        options.AccountEndpoint ??= hostOptions.Value.Application.GetInfrastructureConvention(InfrastructureConventionType.CosmosDbAccountEndpoint);
+                    });
+            }
 
             services.AddSingleton<CosmosClient>(serviceProvider =>
             {
                 var applicationHostEnvironment = serviceProvider.GetRequiredService<IApplicationHostEnvironment>();
                 var credential = serviceProvider.GetRequiredService<TokenCredential>();
+                var options = serviceProvider.GetRequiredService<IOptions<CosmosDbSettings>>();
 
                 var cosmosClientOptions = new CosmosClientOptions
                 {
@@ -37,13 +48,13 @@ namespace Milochau.Core.Cosmos
                     }
                 };
 
-                if (!string.IsNullOrEmpty(settingsValue.ConnectionString))
+                if (!string.IsNullOrEmpty(options.Value.ConnectionString))
                 {
-                    return new CosmosClient(settingsValue.ConnectionString, cosmosClientOptions);
+                    return new CosmosClient(options.Value.ConnectionString, cosmosClientOptions);
                 }
                 else
                 {
-                    return new CosmosClient(settingsValue.AccountEndpoint, credential, cosmosClientOptions);
+                    return new CosmosClient(options.Value.AccountEndpoint, credential, cosmosClientOptions);
                 }
             });
 
