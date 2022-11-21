194ce7298c616bd51cfe9e6bb44e45288214807f
Antoine Milochau <48644548+amilochau@users.noreply.github.com>
2022-11-21 11:35:59 +0100

    Use cancellation tokens in Functions apps

--- src/Libraries Projects/Milochau.Core.Functions/Functions/SystemApplicationFunctions.cs
+++ src/Libraries Projects/Milochau.Core.Functions/Functions/SystemApplicationFunctions.cs
@@ -5,6 +5,7 @@ using Milochau.Core.Abstractions.Models.System;
 using Milochau.Core.Functions.Helpers;
 using System;
 using System.Net;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Milochau.Core.Functions.Functions
@@ -22,34 +23,34 @@ namespace Milochau.Core.Functions.Functions
 
         /// <summary>Get application information</summary>
         [Function("system-application")]
-        public async Task<HttpResponseData> GetInformationAsync([HttpTrigger(AuthorizationLevel.Admin, "get", Route = "system/application/{type}")] HttpRequestData request, string type)
+        public async Task<HttpResponseData> GetInformationAsync([HttpTrigger(AuthorizationLevel.Admin, "get", Route = "system/application/{type}")] HttpRequestData request, string type, CancellationToken cancellationToken)
         {
             return request.Method switch
             {
-                Keys.GetMethod when type.Equals("assembly", StringComparison.OrdinalIgnoreCase) => await GetAssemblyAsync(request),
-                Keys.GetMethod when type.Equals("environment", StringComparison.OrdinalIgnoreCase) => await GetEnvironmentAsync(request),
-                _ => request.WriteEmptyResponseAsync(HttpStatusCode.NotFound),
+                Keys.GetMethod when type.Equals("assembly", StringComparison.OrdinalIgnoreCase) => await GetAssemblyAsync(request, cancellationToken),
+                Keys.GetMethod when type.Equals("environment", StringComparison.OrdinalIgnoreCase) => await GetEnvironmentAsync(request, cancellationToken),
+                _ => request.WriteEmptyResponse(HttpStatusCode.NotFound),
             };
         }
 
         /// <summary>Get application asembly</summary>
-        internal static async Task<HttpResponseData> GetAssemblyAsync(HttpRequestData request)
+        internal static async Task<HttpResponseData> GetAssemblyAsync(HttpRequestData request, CancellationToken cancellationToken)
         {
             var assembly = System.Reflection.Assembly.GetEntryAssembly()!;
             var assemblyResponse = new AssemblyResponse(assembly);
 
             var response = request.CreateResponse();
-            await response.WriteAsJsonAsync(assemblyResponse);
+            await response.WriteAsJsonAsync(assemblyResponse, cancellationToken);
             return response;
         }
 
         /// <summary>Get application environment</summary>
-        internal async Task<HttpResponseData> GetEnvironmentAsync(HttpRequestData request)
+        internal async Task<HttpResponseData> GetEnvironmentAsync(HttpRequestData request, CancellationToken cancellationToken)
         {
             var environmentResponse = new EnvironmentResponse(applicationHostEnvironment);
 
             var response = request.CreateResponse();
-            await response.WriteAsJsonAsync(environmentResponse);
+            await response.WriteAsJsonAsync(environmentResponse, cancellationToken);
             return response;
         }
     }
